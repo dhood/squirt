2f98bdbfdc03b12a563552d3ad8a3972e33368e6
Deanna Hood <deanna.m.hood@gmail.com>
2015-05-06 16:48:42 -0400

    Catch case when empty page

--- mrdeanna.js
+++ mrdeanna.js
@@ -1,12 +1,15 @@
 (function(){
   var article = document.getElementsByTagName('article')[0];
-  for(var i = 0; i < article.children.length; i++) {
-    if (article.children[i].innerText) {
-      var text = article.children[i].innerHTML;
-      var newText = ['<b>', text.slice(0, 1), '</b>', text.slice(1)].join('');
-      article.children[i].innerHTML = newText;
-    } else {
-      console.log(false);
-    } 
-  } 
-})();
+    if (article){
+      for(var i = 0; i < article.children.length; i++) {
+        if (article.children[i].innerText) {
+          var text = article.children[i].innerHTML;
+          var newText = ['<b>', text.slice(0, 1), '</b>', text.slice(1)].join('');
+          article.children[i].innerHTML = newText;
+        } else {
+          console.log(false);
+        } 
+      } 
+    }
+  }
+)();
