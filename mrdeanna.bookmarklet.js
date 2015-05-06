3b5c0198d6c93bdce41e43fb7afb95584d471309
Andrew Drozdov <andrew@mrdrozdov.com>
2015-05-06 17:45:39 -0400

    LOGGING

--- mrdeanna.bookmarklet.js
+++ mrdeanna.bookmarklet.js
@@ -1,15 +1,24 @@
 (function() {
   var mrd = window.mrd = window.mrd || {};
 
+  document.addEventListener('readability.ready', function () {
+    mrd.loaded = {};
+    mrd.loaded.readability = true;
+  }, false);
+
   if (!mrd.readability) {
     mrd.readability = document.createElement('script');
     mrd.readability.src = '//dhood.github.io/squirt/readability.js';
     document.body.appendChild(mrd.readability);
+  } else {
+    console.log("mrd.readability exists");
   }
 
   if (!mrd.mrdeanna) {
     mrd.mrdeanna = document.createElement('script');
     mrd.mrdeanna.src = '//dhood.github.io/squirt/mrdeanna.js';
     document.body.appendChild(mrd.mrdeanna);
+  } else {
+    console.log("mrd.mrdeanna exists");
   }
 })();
--- mrdeanna.js
+++ mrdeanna.js
@@ -1,18 +1,27 @@
-var mrdeanna = function () {
-  var article = document.getElementsByTagName('article')[0];
-  if (article){
-    for(var i = 0; i < article.children.length; i++) {
-      if (article.children[i].innerText) {
-        var text = article.children[i].innerHTML;
-        var newText = ['<b>', text.slice(0, 1), '</b>', text.slice(1)].join('');
-        article.children[i].innerHTML = newText;
-        console.log("Bolded");
-        console.log(newText);
-      } else {
-        console.log(false);
+(function(){
+  var mrdeanna = function () {
+    var article = document.getElementsByTagName('article')[0];
+    console.log("the article is:");
+    console.log(article);
+    if (article){
+      for(var i = 0; i < article.children.length; i++) {
+        if (article.children[i].innerText) {
+          var text = article.children[i].innerHTML;
+          var newText = ['<b>', text.slice(0, 1), '</b>', text.slice(1)].join('');
+          article.children[i].innerHTML = newText;
+          console.log("Bolded");
+          console.log(newText);
+        } else {
+          console.log(false);
+        } 
       } 
-    } 
+    }
   }
-}
 
-document.addEventListener('readability.ready', mrdeanna, false);
+  while (1) {
+    if (window.mrd.loaded.readability) {
+      mrdeanna();
+      break;
+    }
+  }
+})();
