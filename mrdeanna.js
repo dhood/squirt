cc3ec203baaea23d26e1a9ba5a3908df4f056003
Deanna Hood <deanna.m.hood@gmail.com>
2015-05-08 20:23:25 -0400

    Only modify each p element once

--- mrdeanna.js
+++ mrdeanna.js
@@ -1,25 +1,26 @@
 (function(){
+  function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+  }
+
   var mrdeanna = function () {
-    //var article = document.getElementsByTagName('article')[0];
     var articleElements = readability.grabArticle();
     console.log("the article is:");
     console.log(articleElements);
     if (articleElements){
+      var pEls = [];
       for(var j = 0; j < articleElements.length; j++) {
         var articleElement = articleElements[j];
-      var pEls = articleElement.getElementsByTagName("p");
+        pEls.push.apply(pEls, articleElement.getElementsByTagName("p"));
+      }
+      pEls = pEls.filter(onlyUnique);
       for(var i = 0; i < pEls.length; i++) {
         if (pEls[i].innerText) {
           var text = pEls[i].innerHTML;
           var newText = ['<b>', text.slice(0, 1), '</b>', text.slice(1)].join('');
           pEls[i].innerHTML = newText;
-          console.log("Bolded");
-          //console.log(newText);
-        } else {
-          console.log(false);
-        } 
+        }
       } 
-    }
     }
   }
 
